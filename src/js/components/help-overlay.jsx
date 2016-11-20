b36e4b517c6daf37049d258ab0d5c973f9c8e1d3
Tony Wallace <tony@tonywallace.com>
2016-11-20 14:21:29 -0500

    Add README and update help overlay text

--- src/js/components/help-overlay.jsx
+++ src/js/components/help-overlay.jsx
@@ -15,7 +15,8 @@ class HelpOverlay extends Component {
 						<ul>
 							<li>Review the <a href="https://github.com/irritant/serialist-grammar#syntax" target="_blank">serialist-grammar</a> syntax.</li>
 							<li>Select a MIDI output port from the <em>MIDI Port</em> menu (OS X users can use the <em>IAC Driver Bus</em> to route MIDI messages to a DAW or software synth).</li>
-							<li>Type your command in the <em>Command</em> field and click <i className="fa fa-check"></i> to evaluate.</li>
+							<li>Type your command in the <em>Command</em> field and click <i className="fa fa-check"></i> or press <em>Shift-Alt-Return</em> to evaluate and play.</li>
+							<li>Control playback with the transport buttons or by pressing <em>Shift-Alt-Space</em> (play/pause) or <em>Shift-Ctrl-Alt-Space</em> (stop).</li>
 							<li>Modify your command and re-evaluate as often as you like. Your previous commands will be listed in the <em>History</em> field.</li>
 							<li>You can use the <a href="https://github.com/irritant/serialist-grammar#identifiers" target="_blank">serialist-grammar identifier syntax</a> to specify a MIDI channel for each voice. If you don't specify a MIDI channel, Serialist will send on channel 1.</li>
 							<li>After playing, save any interesting commands by copying them from the <em>History</em> field to a text file on your computer.</li>
